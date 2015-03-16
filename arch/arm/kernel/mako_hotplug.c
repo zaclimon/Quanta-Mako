fd660e5943aa65aa0c0f6c557dade349b7003575
franciscofranco <franciscofranco.1990@gmail.com>
2015-03-16 00:27:12 +0000

    mako_hotplug: increase the work schedule time when dual-core or quad-core mode is active, from 1 to 2 seconds
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>

--- arch/arm/kernel/mako_hotplug.c
+++ arch/arm/kernel/mako_hotplug.c
@@ -249,9 +249,9 @@ reschedule:
 	 * This reschedule is specially for cases where the user wants to
 	 * run either dual-core or quad-core permanently - for that reason
 	 * we don't need to run this work every 100ms, but rather just
-	 * once every second
+	 * once every 2 seconds
 	 */
-	queue_delayed_work(wq, &decide_hotplug, HZ);
+	queue_delayed_work(wq, &decide_hotplug, HZ * 2);
 }
 
 static void mako_hotplug_suspend(struct work_struct *work)
